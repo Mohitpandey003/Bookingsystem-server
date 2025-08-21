82cd14d937b3242cf357f031ac2c87e7d6e0312c
Mohitpandey003 <mohitraj2022003@gmail.com>
2025-08-21 13:35:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express= require('express')
+
+const app=express()
+
+const web = require('./routes/web')
+const connectDB = require('./db/connectDB')
+const fileupload = require('express-fileupload')
+const cors=require("cors");
+const cookieParser = require('cookie-parser')
+require('dotenv').config();
+
+
+app.use(cookieParser())
+
+
+app.use(
+    cors({
+        origin:"http://localhost:5173",
+        credentials:true,
+    })
+);
+
+// image upload
+app.use(fileupload({
+    useTempFiles: true,
+
+}))
+
+
+connectDB()
+app.use(express.json()) // to parse JSON bodies
+
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello World!')
+// }) no longer needed
+
+app.use('/api', web)
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on http://localhost:${process.env.PORT}`);
+});
+
